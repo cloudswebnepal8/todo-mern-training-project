81e4287347c5cd3dbb64f571f9125d53ec3bbe0c
cloudswebnepal8 <cloudswebnepal8@gmail.com>
2026-07-22 11:59:44 +0545

    files

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:4000"
+    baseURL: "https://backendwebsite-omkg.onrender.com/"
 });
 
 API.interceptors.request.use(config => {
